b3d7de23b605af7f24bd7c08638555ba5f33ed0f
Binit Kumar Singh <binit@decent.com>
2024-03-23 13:09:10 +0530

    add user login logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root "books#index"
+
+  root "users#home"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
   resources :books
   resources :members
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_194700) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_115506) do
   create_table "book_issues", force: :cascade do |t|
     t.integer "book_id", null: false
     t.integer "member_id", null: false
@@ -39,6 +39,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_194700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "mobile_no"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_issues", "books"
