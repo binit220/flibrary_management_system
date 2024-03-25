82978bf81117cc2c5ae873404594c64ec0f13d25
Binit Kumar Singh <binit@decent.com>
2024-03-25 17:09:54 +0530

    modify form style and add member edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,37 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
   def new
 		@user = User.new
 	end
 	
 	def create
-		byebug
 		@user = User.new(user_params)
-		byebug
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to root_url, notice: "Thank you for signing up!"
 		else
 			render "new"
 		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			redirect_to users_path
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to members_path
 	end
 
 	private
